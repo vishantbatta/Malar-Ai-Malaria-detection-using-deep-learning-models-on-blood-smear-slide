3870af6558c9c5023aeee4b8dd675b773dc85ef7
OVSRIKANTH <62193166+OVSRIKANTH@users.noreply.github.com>
2020-03-31 20:41:51 +0530

    Update Test_Model.py

--- Test_Model.py
+++ Test_Model.py
@@ -5,11 +5,11 @@ model.evaluate_generator(generator=test_batches, steps=None, callbacks=None, max
 
 X_Testing_images =[]
 y_Testing_images =[]
-for features, labels in Testing_images:
+for features, labels in test_batches:
     X_Testing_images.append(features)
     y_Testing_images.append(labels)
 
-    y_pred=model.predict(X_Testing_images, batch_size=50, verbose=0, steps=None, callbacks=None, max_queue_size=10, workers=1, use_multiprocessing=False)    
+y_pred=model.predict(X_Testing_images, batch_size=50, verbose=0, steps=None, callbacks=None, max_queue_size=10, workers=1, use_multiprocessing=False)    
 
 from sklearn.metrics import accuracy_score
 accuracy_score(y_Testing_images,y_pred)*100           #finding accuracy of model
